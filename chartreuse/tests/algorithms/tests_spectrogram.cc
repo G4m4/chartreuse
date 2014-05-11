bd80f25006aced72da7dfef2a839b5a42a3143f9
Gama <misept.dieseneuf@gmail.com>
2014-05-11 23:41:05 +0200

    ALGORITHMS: Add tests for the spectrogram
    Nothing check for now but allows to debug the output data.
    Part of #18.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ chartreuse/tests/algorithms/tests_spectrogram.cc
@@ -0,0 +1,70 @@
+/// @file tests_spectrogram.cc
+/// @brief Chartreuse spectrogram algorithm tests
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of Chartreuse
+///
+/// Chartreuse is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Chartreuse is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "chartreuse/tests/tests.h"
+
+#include "chartreuse/src/algorithms/spectrogram.h"
+
+// Using declarations for tested class
+using chartreuse::algorithms::Spectrogram;
+
+/// @brief Compute the spectrogram for white noise
+TEST(Spectrogram, WhiteNoise) {
+  const unsigned int kWindowLength(1440);
+  const unsigned int kDftLength(2048);
+  const float kSamplingFreq(48000.0f);
+  std::vector<float> out_data(kDftLength);
+
+  Spectrogram spectrogram(kWindowLength, kDftLength, kSamplingFreq);
+
+  unsigned int index(0);
+  while (index < kDataTestSetSize) {
+    std::array<float, chartreuse::kHopSizeSamples> frame;
+    // Fill the frame with random data
+    std::generate(frame.begin(),
+                  frame.end(),
+                  [&] {return kNormDistribution(kRandomGenerator);});
+    spectrogram(&frame[0],
+                &out_data[0]);
+    index += frame.size();
+  }
+}
+
+/// @brief Compute the spectrogram for a sinus
+TEST(Spectrogram, Sin) {
+  const unsigned int kWindowLength(1440);
+  const unsigned int kDftLength(2048);
+  const float kSamplingFreq(48000.0f);
+  std::vector<float> out_data(kDftLength);
+
+  Spectrogram spectrogram(kWindowLength, kDftLength, kSamplingFreq);
+
+  unsigned int index(0);
+  while (index < kDataTestSetSize) {
+    std::array<float, chartreuse::kHopSizeSamples> frame;
+    // Fill the frame with sin data
+    std::copy(&kInSin[index],
+              &kInSin[std::min(index + frame.size(), kDataInSinLength - 1)],
+              frame.begin());
+    spectrogram(&frame[0],
+                &out_data[0]);
+    index += frame.size();
+  }
+}

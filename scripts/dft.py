b6d1c3693b8fdd476edbf79df1d3e04ff823e175
Gama <misept.dieseneuf@gmail.com>
2014-05-12 17:40:05 +0200

    SCRIPTS: Fix missing license
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- scripts/dft.py
+++ scripts/dft.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 '''
 @file dft.py
-@brief Sandbox scripts for DFT algorithm tests
+@brief Sandbox for DFT algorithms tests
+@author gm
+@copyright gm 2014
+
+This file is part of Chartreuse
+
+Chartreuse is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Chartreuse is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import numpy
 import pylab

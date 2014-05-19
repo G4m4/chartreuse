3267b88fe0a18ceff11a63be9600ebec1836b2f1
Gama <misept.dieseneuf@gmail.com>
2014-05-19 19:30:38 +0200

    MANAGER: Add a performance test
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- chartreuse/tests/manager/tests_manager.cc
+++ chartreuse/tests/manager/tests_manager.cc
@@ -54,3 +54,31 @@ TEST(Manager, WhiteNoise) {
     index += frame.size();
   }
 }
+
+/// @brief Compute all descriptors for white noise
+TEST(Manager, Perf) {
+  const float kSamplingFreq(48000.0f);
+
+  Manager manager(kSamplingFreq);
+
+  for (unsigned int descriptor_idx(0);
+       descriptor_idx < kCount;
+       ++descriptor_idx) {
+    manager.EnableDescriptor(static_cast<Type>(descriptor_idx), true);
+  }
+
+  std::vector<float> out_data(manager.DescriptorsOutputSize());
+
+  std::size_t index(0);
+  while (index < kDataPerfSetSize) {
+    std::array<float, chartreuse::kHopSizeSamples> frame;
+    // Fill the frame with random data
+    std::generate(frame.begin(),
+                  frame.end(),
+                  [&] {return kNormDistribution(kRandomGenerator);});
+    manager(&frame[0],
+            frame.size(),
+            &out_data[0]);
+    index += frame.size();
+  }
+}

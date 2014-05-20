9dd1fe3ac1cc78504ca4c64a7e70ff085f79dc04
Gama <misept.dieseneuf@gmail.com>
2014-05-20 15:35:23 +0200

    ALGORITHMS: Add 2 new algorithms for spectrum power computation
    Part of #29 - those are required by ASC and ASS.
    Note that these algorithms are identical - see related issue #31
    for future enhancement tracking.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ chartreuse/src/algorithms/dftpower.cc
@@ -0,0 +1,75 @@
+/// @file dftpower.h
+/// @brief DFT squared magnitude algorithm implementation
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of Chartreuse
+///
+/// Chartreuse is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Chartreuse is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "chartreuse/src/algorithms/dftpower.h"
+
+#include <cmath>
+
+#include <algorithm>
+
+#include "chartreuse/src/common.h"
+#include "chartreuse/src/algorithms/algorithms_common.h"
+#include "chartreuse/src/manager/manager.h"
+
+namespace chartreuse {
+namespace algorithms {
+
+DftPower::DftPower(manager::Manager* manager)
+    : Descriptor_Interface(manager),
+      // TODO(gm): remove the magic, replace it by actual norm computation
+      normalization_factor_(2.0f / (manager->DftLength() * 571.865f)) {
+  CHARTREUSE_ASSERT(normalization_factor_ > 0.0f);
+}
+
+void DftPower::operator()(const float* const frame,
+                          const std::size_t frame_length,
+                          float* const data) {
+  CHARTREUSE_ASSERT(frame != nullptr);
+  CHARTREUSE_ASSERT(frame_length > 0);
+  CHARTREUSE_ASSERT(data != nullptr);
+
+  // Get the Dft of the frame
+  const float* const dft_data(manager_->GetDescriptor(
+                                manager::DescriptorId::kDft,
+                                &frame[0],
+                                frame_length));
+  // Retrieve the normalized squared magnitude of the data
+  for (std::size_t i(0);
+       i < manager_->GetDescriptorSize(manager::DescriptorId::kDft);
+       i += 2) {
+    data[i / 2] = dft_data[i] * dft_data[i];
+    data[i / 2] += dft_data[i + 1] * dft_data[i + 1];
+    data[i / 2] *= normalization_factor_;
+  }
+}
+
+descriptors::Descriptor_Meta DftPower::Meta(void) const {
+  return descriptors::Descriptor_Meta(
+    // Only real data
+    manager_->DftLength() / 2 + 1,
+    // Actually the input frame_length...
+    0.0f,
+    // Actually the input frame_length...
+    static_cast<float>(manager_->DftLength() * manager_->DftLength())
+    * normalization_factor_);
+}
+
+}  // namespace algorithms
+}  // namespace chartreuse
---
+++ chartreuse/src/algorithms/spectrogrampower.h
@@ -0,0 +1,55 @@
+/// @file spectrogrampower.h
+/// @brief Spectrogram squared magnitude algorithm declarations
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of Chartreuse
+///
+/// Chartreuse is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Chartreuse is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef CHARTREUSE_SRC_ALGORITHMS_SPECTROGRAMPOWER_H_
+#define CHARTREUSE_SRC_ALGORITHMS_SPECTROGRAMPOWER_H_
+
+#include "chartreuse/src/common.h"
+#include "chartreuse/src/descriptors/descriptor_interface.h"
+
+namespace chartreuse {
+namespace algorithms {
+
+/// @brief Compute the Spectrogram squared magnitude.
+///
+/// Note that the output is normalized, the default normalization being:
+/// 2.0f / (dft length * input signal window length)
+class SpectrogramPower : public descriptors::Descriptor_Interface {
+ public:
+  explicit SpectrogramPower(manager::Manager* manager);
+
+  /// Output size: dft_length / 2 + 1
+  void operator()(const float* const frame,
+                  const std::size_t frame_length,
+                  float* const data);
+
+  descriptors::Descriptor_Meta Meta(void) const;
+
+ private:
+  // No assignment operator for this class
+  SpectrogramPower& operator=(const SpectrogramPower& right);
+
+  const float normalization_factor_;  ///< Normalization to be applied on the output
+};
+
+}  // namespace algorithms
+}  // namespace chartreuse
+
+#endif  // CHARTREUSE_SRC_ALGORITHMS_SPECTROGRAMPOWER_H_

543f5ce1d3e68c34cf3d989a2bbc2ea22abcf2c8
Gama <misept.dieseneuf@gmail.com>
2014-06-17 11:41:11 +0200

    DESCRIPTORS: Add basic source structure for AudioHarmonicity ULH
    Part of #40.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ chartreuse/src/descriptors/audioupperlimitofharmonicity.cc
@@ -0,0 +1,55 @@
+/// @file audioupperlimitofharmonicity.cc
+/// @brief AudioUpperLimitOfHarmonicity "ULH" part descriptor implementation
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of Chartreuse
+///
+/// Chartreuse is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Chartreuse is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "chartreuse/src/descriptors/audioupperlimitofharmonicity.h"
+
+#include "chartreuse/src/manager/manager.h"
+
+namespace chartreuse {
+namespace descriptors {
+
+AudioUpperLimitOfHarmonicity::AudioUpperLimitOfHarmonicity(manager::Manager* manager)
+    : Descriptor_Interface(manager) {
+  // Nothing to do here for now
+}
+
+void AudioUpperLimitOfHarmonicity::operator()(float* const output) {
+  IGNORE(output);
+}
+
+void AudioUpperLimitOfHarmonicity::Process(const float* const autocorrelation,
+                                           const float estimated_lag,
+                                           const unsigned int min_lag,
+                                           float* const output) {
+  IGNORE(autocorrelation);
+  IGNORE(estimated_lag);
+  IGNORE(min_lag);
+  IGNORE(output);
+}
+
+Descriptor_Meta AudioUpperLimitOfHarmonicity::Meta(void) const {
+  return Descriptor_Meta(
+    1,
+    -1.0f,
+    1.0f);
+}
+
+}  // namespace descriptors
+}  // namespace chartreuse
---
+++ chartreuse/src/descriptors/audioupperlimitofharmonicity.h
@@ -0,0 +1,54 @@
+/// @file audioupperlimitofharmonicity.h
+/// @brief AudioHarmonicity "ULH" part descriptor declaration
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of Chartreuse
+///
+/// Chartreuse is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Chartreuse is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Chartreuse.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef CHARTREUSE_SRC_DESCRIPTORS_AUDIOUPPERLIMITOFHARMONICITY_H_
+#define CHARTREUSE_SRC_DESCRIPTORS_AUDIOUPPERLIMITOFHARMONICITY_H_
+
+#include "chartreuse/src/descriptors/descriptor_interface.h"
+
+namespace chartreuse {
+namespace descriptors {
+
+/// @brief AudioHarmonicity "ULH" part descriptor: retrieve the value in Hz
+/// after which the frame is no longer estimated "harmonic"
+class AudioUpperLimitOfHarmonicity : public Descriptor_Interface {
+ public:
+  explicit AudioUpperLimitOfHarmonicity(manager::Manager* manager);
+
+  void operator()(float* const output);
+
+  /// @brief Independent process method: this is where the actual computation
+  /// is done, to be used in a "raw" way when no manager is available
+  void Process(const float* const autocorrelation,
+               const float estimated_lag,
+               const unsigned int min_lag,
+               float* const output);
+
+  Descriptor_Meta Meta(void) const;
+
+ private:
+  // No assignment operator for this class
+  AudioUpperLimitOfHarmonicity& operator=(const AudioUpperLimitOfHarmonicity& right);
+};
+
+}  // namespace descriptors
+}  // namespace chartreuse
+
+#endif  // CHARTREUSE_SRC_DESCRIPTORS_AUDIOUPPERLIMITOFHARMONICITY_H_

33137025b069c13351b8d9ac6d180f22d49c5d91
Gama <misept.dieseneuf@gmail.com>
2014-06-23 15:36:10 +0200

    ANALYZER/INTERFACE: Add an actual test for consistency accross subframes
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- chartreuse/tests/interface/tests_analyzer.cc
+++ chartreuse/tests/interface/tests_analyzer.cc
@@ -27,10 +27,10 @@ using chartreuse::interface::Analyzer;
 // Using declarations for related classes
 using chartreuse::interface::kAvailableDescriptors;
 
-/// @brief Feed the analyzer with white noise
-TEST(Analyzer, WhiteNoise) {
+/// @brief Feed the analyzer with white noise, check output range
+TEST(Analyzer, WhiteNoiseRange) {
   const float kSamplingFreq(48000.0f);
-  const unsigned int kFrameLength(480);
+  const unsigned int kFrameLength(chartreuse::kHopSizeSamples * 3);
   const unsigned int kExpectedSubframesCount(kFrameLength
                                              / chartreuse::kHopSizeSamples);
 
@@ -60,6 +60,50 @@ TEST(Analyzer, WhiteNoise) {
         const float kValue(out_data[kActualIdx]);
         EXPECT_GE(1.0f, kValue);
         EXPECT_LE(0.0f, kValue);
+      }
+    }  // subframe_idx
+    index += frame.size();
+  }
+}
+
+/// @brief Feed the analyzer with white noise,
+/// check consitency accross subframes
+TEST(Analyzer, WhiteNoiseConsistency) {
+  const float kSamplingFreq(48000.0f);
+  const unsigned int kFrameLength(chartreuse::kHopSizeSamples * 3);
+  const unsigned int kExpectedSubframesCount(kFrameLength
+                                             / chartreuse::kHopSizeSamples);
+  const float kEpsilon(1e-3f);
+
+  Analyzer analyzer(kSamplingFreq);
+
+  std::vector<float> out_data(kAvailableDescriptors.size()
+                              * kExpectedSubframesCount);
+
+  std::size_t index(0);
+  while (index < kDataTestSetSize) {
+    std::vector<float> frame(kFrameLength);
+    // Fill the frame with random data
+    std::generate(frame.begin(),
+                  frame.end(),
+                  [&] {return kNormDistribution(kRandomGenerator);});
+    analyzer.Process(&frame[0],
+                     frame.size(),
+                     &out_data[0]);
+    // Expecting each subframe to be very close to the second one
+    // The first one is ignored due to the "empty first buffer" effect
+    for (unsigned int subframe_idx(2);
+         subframe_idx < kExpectedSubframesCount;
+         ++subframe_idx) {
+      for (unsigned int descriptor_idx(0);
+        descriptor_idx < kAvailableDescriptors.size();
+        ++descriptor_idx) {
+        const unsigned int kActualIdx(subframe_idx * kAvailableDescriptors.size()
+          + descriptor_idx);
+        const float kValue(out_data[kActualIdx]);
+        EXPECT_NEAR(out_data[kAvailableDescriptors.size() + descriptor_idx],
+                    kValue,
+                    kEpsilon);
       }
     }  // subframe_idx
     index += frame.size();
